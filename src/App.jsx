158211e8ea7807dd079eb3aeb53418ad14d012f9
leandrofariasfl <leandrofariasfl28884@gmail.com>
2026-05-26 10:16:23 -0300

    feat(layout): integra o grid de projetos e a seção de skills na visualização principal

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import { MdPortableWifiOff } from 'react-icons/md';
 import Project from './components/Project.jsx';
+import Skills from './components/Skills.jsx';
 
 function App() {
   // Lista simulada de projetos (isso é bem comum no React)
-  const projetos = [
+ /* const projetos = [
     { id: 1, titulo: "Meu Primeiro Link", desc: "Um projeto simples usando HTML e CSS." },
     { id: 2, titulo: "App de Clima", desc: "Consumindo uma API de tempo com JavaScript." },
     { id: 3, titulo: "Este Portfólio", desc: "Criado com React e Tailwind CSS!" },
-  ];
+  ]; */
 
   return (
     <div className="min-h-screen bg-slate-950 text-slate-100 antialiased">
@@ -20,6 +21,7 @@ function App() {
       <Navbar />
       <Hero />
       <Project />
+      <Skills />
     </div>
   );
 }
